89953f3dbd51823744d664431686e82c5262e5b3
YeongTaek <yeongtaek2109@gmail.com>
2024-06-11 18:25:12 +0900

    feat : boardsPage 구성

---
+++ next_mission/pages/boards/index.tsx
@@ -0,0 +1,9 @@
+import BestArticlesSection from '@/components/boards/BestArticlesSection';
+
+export default function BoardsPage() {
+  return (
+    <div className="flex flex-col p-4 gap-10">
+      <BestArticlesSection />
+    </div>
+  );
+}
