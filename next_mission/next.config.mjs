7c235ce30b6f8188ee820dc24c36b52aa5ddf167
YeongTaek <yeongtaek2109@gmail.com>
2024-06-13 12:22:26 +0900

    fix : 일반 게시글 받아오기 위한 ㅓㄹ정

--- next_mission/next.config.mjs
+++ next_mission/next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
   // 외부 이미지 주소
   // 작성하지 않으면, <ForwardRef> component: at eval 에러발생
   images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    domains: [
+      'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      'example.com',
+    ],
   },
 
   // SVG 이미지를 리액트 컴포넌트로 변환해주는 라이브러리 설정 추가
