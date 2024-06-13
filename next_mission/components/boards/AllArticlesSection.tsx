ebc3c6b9474716cdac67188faa130bfd4b72fbe3
YeongTaek <yeongtaek2109@gmail.com>
2024-06-13 14:02:10 +0900

    feat : 일반 게시글 받아오기 구현

--- next_mission/components/boards/AllArticlesSection.tsx
+++ next_mission/components/boards/AllArticlesSection.tsx
@@ -1,14 +1,23 @@
 import { Article } from '@/types/articleTypes';
 import Button from '../common/Button';
 import SearchBar from '../ui/SearchBar';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import EmptyArticleList from '../ui/EmptyArticleList';
+import ArticleItem from './ArticleItem';
 
 interface AllArticlesSectionProps {
   initialArticles: Article[];
 }
 
-const AllArticlesSection: React.FC<AllArticlesSectionProps> = () => {
+const AllArticlesSection: React.FC<AllArticlesSectionProps> = ({
+  initialArticles,
+}) => {
+  const [articles, setArticles] = useState(initialArticles);
+  const [orderBy, setOrderBy] = useState('recent');
+
   const router = useRouter();
+  const keyword = (router.query.q as string) || '';
 
   const handleInputKeyword = (inputKeyword: string) => {
     const query = { ...router.query };
@@ -23,6 +32,20 @@ const AllArticlesSection: React.FC<AllArticlesSectionProps> = () => {
     });
   };
 
+  useEffect(() => {
+    const fetchArticles = async () => {
+      let url = `https://panda-market-api.vercel.app/articles?orderBy=${orderBy}`;
+      if (keyword.trim()) {
+        url += `&keyword=${encodeURIComponent(keyword)}`;
+      }
+      const res = await fetch(url);
+      const data = await res.json();
+      setArticles(data.list);
+    };
+
+    fetchArticles();
+  }, [keyword]);
+
   return (
     <div className="flex flex-col gap-6 w-[1200px] m-auto">
       <div className="flex justify-between">
@@ -33,6 +56,14 @@ const AllArticlesSection: React.FC<AllArticlesSectionProps> = () => {
       <div>
         <SearchBar onSearch={handleInputKeyword} />
       </div>
+
+      {articles.length
+        ? articles.map((article) => (
+            <ArticleItem key={`article-${article.id}`} article={article} />
+          ))
+        : keyword && (
+            <EmptyArticleList text={`'${keyword}'로 검색된 결과가 없습니다.`} />
+          )}
     </div>
   );
 };
