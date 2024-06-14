887f471bcbb575e4220e192715fc38961878f6e6
YeongTaek <yeongtaek2109@gmail.com>
2024-06-14 20:49:06 +0900

    feat : 상품 댓글 기능 구현

--- next_mission/components/items/ItemCommentSection.tsx
+++ next_mission/components/items/ItemCommentSection.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react';
+import Comment from './Comment';
 
 const COMMENT_PLACEHOLDER =
   '개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다.';
@@ -33,6 +34,8 @@ const ItemCommentSection: React.FC<ItemCommentSectionProps> = ({
       >
         등록
       </button>
+
+      <Comment productId={productId} />
     </section>
   );
 };
